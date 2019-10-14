cd22e3e3598bc13609a74fac6d5f4962f1e76124
Joni Jimenez <jonimarie.jimenez@gmail.com>
2019-10-14 11:52:54 +0800

    Add Task

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View
 } from 'react-native';
@@ -31,10 +32,6 @@ class App extends Component {
         ]
       },
       {
-        label: 'Shower',
-        value: false
-      },
-      {
         label: 'Exercise',
         value: [{
             label: 'Stretching',
@@ -47,16 +44,48 @@ class App extends Component {
         ],
       },
       {
-        label: 'Eat',
+        label: 'Sleep',
         value: true
+      },
+      {
+        label: 'Browse Internet',
+        value: false
+      },
+      {
+        label: 'Shower',
+        value: false
+      },
+      {
+        label: 'Eat',
+        value: [{
+            label: 'Rice',
+            value: false
+          },
+          {
+            label: 'Soup',
+            value: true
+          },
+          {
+            label: 'Chicken',
+            value: true
+          },
+          {
+            label: 'Vegetable',
+            value: true
+          },
+          {
+            label: 'Beef',
+            value: true
+          }
+        ],
       }
     ]
 
     this.state = {
       data,
-      background: 'blue'
+      textInputModal: true,
+      addTask: ''
     }
-
   }
 
   handlePress = (index) => {
@@ -71,6 +100,31 @@ class App extends Component {
     data[index].value[index2].value = data[index].value[index2].value ? false : true;
 
     this.setState({data});
+  }
+
+  handleModal = () => {
+    this.setState({
+      textInputModal: this.state.textInputModal ? false : true
+    })
+  }
+
+  handleTextChange = (text) => {
+    this.setState({
+      addTask: text
+    })
+  }
+
+  handleSave = () => {
+    let data = this.state.data;
+    data.push({
+      label: this.state.addTask,
+      value: false
+    });
+
+    this.setState({
+      data,
+      textInputModal: false
+    })
   }
 
   render() {
@@ -136,63 +190,168 @@ class App extends Component {
         <ScrollView contentContainerStyle={styles.scrollView}>
           {data}
         </ScrollView>
+
+        {/* ADD BUTTON */}
+        <TouchableOpacity onPress={this.handleModal.bind(this)} style={styles.addButton}>
+          <FontAwesome5 name={'plus'} style={styles.plusButton}/>
+        </TouchableOpacity>
+
+        { this.state.textInputModal &&
+          <View style={styles.modalContainer}>
+            <View style={styles.inputContainer}>
+              <Text style={styles.addTaskHeader}>Add Task +</Text>
+              <TextInput
+                style={styles.textInput}
+                autoFocus={true}
+                multiline={true}
+                onChangeText={this.handleTextChange}
+              />
+              <View style={styles.modalActions}>
+                <TouchableOpacity
+                  onPress={this.handleModal}
+                  style={styles.cancelButton}
+                >
+                  <Text style={styles.cancelText}>Cancel</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.saveButton}
+                  onPress={this.handleSave}
+                >
+                  <Text style={styles.saveText}>Save</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        }
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    view: {
-      flex: 1
-    },
-    headerTitle: {
-      color: '#fff',
-      fontSize: 20
-    },
-    headerIcon: {
-      fontSize: 20,
-      color: '#fff'
-    },
-    headerContainer: {
-      flexDirection: 'row',
-      backgroundColor: '#ff6347',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      paddingHorizontal: 20,
-      paddingVertical: 15
-    },
-    checkBox: {
-      fontSize: 20,
-      color: '#ff6347',
-      marginRight: 15
-    },
-    strikeThrough: {
-      textDecorationLine: 'line-through',
-      textDecorationStyle: 'solid',
-      color: 'gray'
-    },
-    itemContainer: {
-      flex: 1,
-      flexDirection: 'row',
-      paddingHorizontal: 15,
-      paddingVertical: 10,
-      borderTopColor: '#fbceb1',
-      borderTopWidth: 1,
-    },
-    categoryContainer: {
-      paddingHorizontal: 30
-    },
-    itemCategoryContainer: {
-      backgroundColor: '#fbceb1',
-      paddingHorizontal: 15,
-      paddingVertical: 5,
-      borderBottomColor: '#ff8c69',
-      borderBottomWidth: 1,
-    },
-    itemCategory: {
-      fontWeight: 'bold'
-    }
-
+  view: {
+    flex: 1
+  },
+  headerTitle: {
+    color: '#fff',
+    fontSize: 20
+  },
+  headerIcon: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#ff6347', // tomato
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 15
+  },
+  checkBox: {
+    fontSize: 20,
+    color: '#ff6347',
+    marginRight: 15
+  },
+  strikeThrough: {
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid',
+    color: 'gray'
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderTopColor: '#fbceb1',  // apricot
+    borderTopWidth: 1,
+  },
+  categoryContainer: {
+    paddingHorizontal: 30
+  },
+  itemCategoryContainer: {
+    backgroundColor: '#fbceb1',
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    borderBottomColor: '#ff8c69',  // salmon
+    borderBottomWidth: 1,
+  },
+  itemCategory: {
+    fontWeight: 'bold'
+  },
+  textInput: {
+    borderColor: '#ff6347',
+    borderWidth: 1,
+    paddingHorizontal: 15,
+    height: 40,
+    paddingVertical: 0
+  },
+  addButton: {
+    backgroundColor: '#ff6347',
+    position: 'absolute',
+    bottom: 15,
+    right: 10,
+    paddingVertical: 13,
+    paddingHorizontal: 15,
+    borderRadius: 50,
+    zIndex: 4
+  },
+  plusButton: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  modalContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  inputContainer: {
+    backgroundColor: '#fafafa',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    width: '80%',
+    borderRadius: 5
+  },
+  addTaskHeader: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  textInput: {
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    paddingVertical: 0,
+  },
+  modalActions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 10
+  },
+  cancelButton: {
+    borderColor:'#ff6347',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 2
+  },
+  cancelText: {
+    color: '#ff6347'
+  },
+  saveButton: {
+    backgroundColor:'#ff6347',
+    borderWidth: 1,
+    borderColor: '#ff6347',
+    borderRadius: 5,
+    paddingHorizontal: 15,
+    paddingVertical: 2
+  },
+  saveText: {
+    color: '#fff'
+  }
 });
 
 export default App;
